9edb8e979b395a614a094a4b5f733409d2c396d9
OumaimaNasmi <80533463+OumaimaNasmi@users.noreply.github.com>
2021-03-17 23:38:21 +0100

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -3,13 +3,25 @@
 """
 Module the sum of two arrays
 """
-
-
 def add_arrays(arr1, arr2):
     """
-    Needs a matrix as input
-    Returns the sum of two arrays a a list
+    Function that add two arrays elemet-wise.
+    Parameters:
+    - arr1 (list of ints/floats): first list.
+    - arr2 (list of ints/floats): second list.
+    Return:
+     A new list with the first list added second list,
+     if the shape of the lists are not the same, return None.
     """
+
+    # comparing the shape
     if len(arr1) != len(arr2):
         return None
-    return [(arr1[i] + arr2[i]) for i in range(len(arr1))]
+
+    # the new list for the add of the lists
+    add = []
+    # cycle for take number by number
+    for i in range(len(arr1)):
+        add.append(arr1[i] + arr2[i])
+
+    return add
